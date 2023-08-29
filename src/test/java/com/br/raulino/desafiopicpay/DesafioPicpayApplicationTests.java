dc2b5e6a05720e2d1b5c8f78c0f9ba701a97b54d
R4UL1N0 <raulino.storage@gmail.com>
2023-08-29 16:29:51 -0300

    iniciando projeto spring

---
+++ src/test/java/com/br/raulino/desafiopicpay/DesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.raulino.desafiopicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
