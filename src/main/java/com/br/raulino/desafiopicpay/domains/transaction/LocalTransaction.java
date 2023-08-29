8585a6e7ea5cc6732abaab1dc85d8c987bdbe08b
R4UL1N0 <raulino.storage@gmail.com>
2023-08-29 16:43:01 -0300

    criação das entidades e seus enums

---
+++ src/main/java/com/br/raulino/desafiopicpay/domains/transaction/LocalTransaction.java
@@ -0,0 +1,42 @@
+package com.br.raulino.desafiopicpay.domains.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.br.raulino.desafiopicpay.domains.user.LocalUser;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "local_transaction")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class LocalTransaction {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private LocalUser sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private LocalUser receiver;
+
+    private BigDecimal amount;
+
+    private LocalDateTime timestamp;
+}
---
+++ src/main/java/com/br/raulino/desafiopicpay/domains/user/UserType.java
@@ -0,0 +1,6 @@
+package com.br.raulino.desafiopicpay.domains.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
